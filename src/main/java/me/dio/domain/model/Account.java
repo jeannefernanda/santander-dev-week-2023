57cb6e73d7cf30f1416c8788f9ab4146b89c0bf9
Jeanne <jeannefernanda.m@gmail.com>
2024-07-03 18:06:39 -0400

    criando o perfil de execução dev

--- src/main/java/me/dio/domain/model/Account.java
+++ src/main/java/me/dio/domain/model/Account.java
@@ -12,9 +12,9 @@ public class Account {
     @Column(unique = true)
     private String number;
     private String agency;
-    @Column(name = "additional_limit",scale = 13, precision = 2)
+    @Column(scale = 13, precision = 2)
     private BigDecimal balance;
-    @Column( scale = 13, precision = 2)
+    @Column(name = "additional_limit",  scale = 13, precision = 2)
     private BigDecimal limit;
 
     public Long getId() {
